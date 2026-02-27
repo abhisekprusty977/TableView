f74725f81b5d68b69584d2eb048f1b81ff197ffd
abhisekprusty977 <abhisekprusty977@gmail.com>
2026-02-27 23:40:51 +0530

    Implement TableView in iOS

---
+++ TableView/TableView/HamburgerViewController.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+protocol HamburgerViewControllerDelegate {
+    func hideHamburgerMenu()
+}
+class HamburgerViewController: UIViewController {
+    
+    var delegate : HamburgerViewControllerDelegate?
+    
+    @IBOutlet weak var profilePictureImage: UIImageView!
+    @IBOutlet weak var mainBackgroundView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setupHamburgerUI()
+        // Do any additional setup after loading the view.
+    }
+    
+    private func setupHamburgerUI()
+    {
+        self.mainBackgroundView.layer.cornerRadius = 40
+        self.mainBackgroundView.clipsToBounds = true
+        
+        self.profilePictureImage.layer.cornerRadius = 40
+        self.profilePictureImage.clipsToBounds = true
+    }
+    
+    @IBAction func clickedOnButton(_ sender: Any) {
+        self.delegate?.hideHamburgerMenu()
+    }
+}
---
+++ TableView/TableView/MovieTableViewCell.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var backView: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    @IBOutlet weak var profilePicImage: UIImageView!
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
